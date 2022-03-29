5d880b73188b7bf7f3de28be6079898ce0ce9ef3
Rizkymsayaf <rizkymsayafif7@gmail.com>
2022-03-29 08:56:05 +0700

    sidebar

--- resources/views/dashboard/laporan/cetak_anak.blade.php
+++ resources/views/dashboard/laporan/cetak_anak.blade.php
@@ -1,5 +1,5 @@
-@extends('dashboard.layouts.main')
-@section('container')
+
+
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
   </div>
 
@@ -45,4 +45,3 @@
       </body>
     </table>
   </div>
-@endsection
